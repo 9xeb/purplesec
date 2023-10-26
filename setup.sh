e79672ef2fcdc6c07151ecfea7eaa77b89728715
9xeb <9xeb@example.com>
2023-10-26 19:13:29 +0200

    Add setup script for ubuntu based systems

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# TODO: move this to a Dockerfile, so that a container is spawned to managing the cluster
+
+wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq
+
+apt update && apt install jq ansible
+
+ansible-galaxy collection install ansible.posix
