a1232a1c4c627f0acc2612b7aa3e25a10ff99dbc
9xeb <9xeb@example.com>
2023-10-26 19:19:15 +0200

    Add nano to setup dependencies

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@
 
 wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq
 
-apt update && apt install jq ansible
+apt update && apt install nano jq ansible
 
 ansible-galaxy collection install ansible.posix
